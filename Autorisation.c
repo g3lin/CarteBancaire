b279f07cc1dd657e6740db2728e4d9f7003a95c2
Antoine GÉLIN <antoine.gelin@edu.esiee.fr>
2019-04-09 07:21:01 +0000

    Fix Autorisation.c
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_116_gelina@7 9a8ef27d-e0b1-43d6-9d9f-2e132b02e8de

---
+++ Autorisation.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "lectureEcriture.h"
+#include "message.h"
+
+/**
+ * Programme d'autorisation des transactions
+ */
+int main(int argc, char **argv)
+{   
+    
+    
+
+    return 0;
+}
