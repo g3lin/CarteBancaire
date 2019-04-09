ba221726cbd3da98ff30fedfd0263cd89a864e64
Antoine GÉLIN <antoine.gelin@edu.esiee.fr>
2019-04-09 08:00:20 +0000

    Add working autorisation server (fixed soldeSurCompte = 999999)
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_116_gelina@9 9a8ef27d-e0b1-43d6-9d9f-2e132b02e8de

--- Autorisation.c
+++ Autorisation.c
@@ -12,7 +12,81 @@
  */
 int main(int argc, char **argv)
 {   
+    int fd_DemandeAuto = atoi(argv[1]);
+    if (fd_DemandeAuto < 0) {
+        perror("Autorisation.c - FD demande autorisation invalide");
+        exit(0);
+    }
+
+    int fd_ReponseAuto = atoi(argv[2]);
+    if (fd_ReponseAuto < 0) {
+        perror("Autorisation.c - FD reponse autorisation invalide");
+        exit(0);
+    }
+
+    // LECTURE DU MESSAGE DE DEMANDE D'AUTORISATION
+
+    char *messageAutorisation = litLigne(fd_DemandeAuto);
+    if (messageAutorisation == NULL) {
+        perror("Autorisation - Demande d'autorisation LitLigne non valide");
+        exit(0);
+    }
     
+    char emetteur[255], type[255], valeur[255];
+    int decoupeOk;
+
+    decoupeOk = decoupe(messageAutorisation, emetteur, type, valeur);
+    if (!decoupeOk) {
+        printf("Erreur de découpage!!\n");
+        exit(0);
+    }
+
+
+
+    // LECTURE DE LA BASE DE CARTES
+
+    // int fd_ListeCartes = open("./ListeCartes.txt", O_RDONLY);
+    // if (fd < 0) {
+    //     perror("Autorisation - ./ListeCartes.txt invalide");
+    //     exit(0);
+    // }
+
+    // char *c = LitLigne(fd_ListeCartes);
+    // if (c == NULL) {
+    //     perror("Autorisation - ./ListeCartes.txt LitLigne non valide");
+    //     exit(0);
+    // }
+
+    int soldeSurCompte = 999999;
+    char* messageReponseAutorisation;
+
+
+    // LE PAIEMENT EST AUTORISE
+    if(soldeSurCompte - atoi(valeur) >= 0 ){
+        
+        messageReponseAutorisation = message(emetteur,"Reponse","1");
+
+        int err = ecritLigne(fd_ReponseAuto, messageReponseAutorisation);
+        if (err == 0) {
+            perror("Autorisation- ecritLigne Réponse Autorisation (valide)");
+            exit(0);
+        }
+        
+        close(fd_ReponseAuto);
+    }
+
+    // PAIEMENT REFUSE CAR PAS LE SOLDE REQUIS
+    else{
+        messageReponseAutorisation = message(emetteur,"Reponse","0");
+
+        int err = ecritLigne(fd_ReponseAuto, messageReponseAutorisation);
+        if (err == 0) {
+            perror("Autorisation- ecritLigne Réponse Autorisation (invalide)");
+            exit(0);
+        }
+        
+        close(fd_ReponseAuto);
+    }
     
 
     return 0;
