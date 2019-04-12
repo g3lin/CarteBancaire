274dd1d95c5ee9280bdde4cbee62a370b5d22e03
Baptiste ADAM <baptiste.adam@edu.esiee.fr>
2019-04-12 15:26:30 +0000

    Version tous les threads se lancent correctement decoupe (du fichier lectureEcriture.h) n'est pas reconnue
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_116_gelina@24 9a8ef27d-e0b1-43d6-9d9f-2e132b02e8de

--- Acquisition.c
+++ Acquisition.c
@@ -13,11 +13,6 @@
 
 //---------------------------------------------------------------------- 
 typedef struct {
-    int fd_auto;
-    int indice_fd_terminal;
-} arg_thread;
-
-typedef struct {
     //infos pour terminal
     int fd_fromTerminal;
     int fd_toTerminal;
@@ -48,7 +43,8 @@ typedef struct {
  */
 
 //---------------------------------------------------------------------- 
-// void *thread_LectureDemande(void *arg){
+void *thread_LectureDemande(void *arg){
+    printf("thread Terminal\n");
 //     arg_thread_T *true_args = arg;
 //     int fd_toAuto = true_args->fd_toAuto;
 //     int fd_fromTerminal = true_args->fd_fromTerminal;
@@ -92,28 +88,26 @@ typedef struct {
 //         }
 //         printf("Serveur Acquisition : message envoyé\n");
 //     }
-// }
+}
 //---------------------------------------------------------------------- 
 
 void *thread_LectureReponse(void *arg){
-    arg_thread_A *trie_arg = arg;
-    printf("coucou : %d\n",trie_arg->fd_fromAuto);
-    // arg_thread_A *true_arg = arg;
-    // int fd_fromAuto = true_arg->fd_fromAuto;
-    // int* Tab_fd_Term = true_arg->Tab_fd_Term;
-    // char* tab_cb = true_arg->tab_cb;
+    arg_thread_A *true_arg = arg;
+    int fd_fromAuto = true_arg->fd_fromAuto;
+    int* Tab_fd_Term = true_arg->Tab_fd_Term;
+    char* tab_cb = true_arg->tab_cb;
 
-    // int fd_toTerminal;
-    // char* rep;
-    // int err;
+    int fd_toTerminal;
+    char* rep;
+    int err;
 
-    // char emetteur[255], type[255], valeur[255];
-    // int decoupeOk;
+    char emetteur[255], type[255], valeur[255];
+    int decoupeOk;
 
-
-    // int fd_pas_trouve = 1;
-    // int i = 0;
-    // //---------------------------------------------------------------------- 
+    int fd_pas_trouve = 1;
+    int i = 0;
+    //---------------------------------------------------------------------- 
+    printf("thread autorisation\n");
     // while(1){
     //     // 1- On attend la reponse du serveur d'autorisation
     //     rep = litLigne(fd_fromAuto);
@@ -131,13 +125,15 @@ void *thread_LectureReponse(void *arg){
     //         //printf("%s", messageAutorisation);
     //         exit(0);
     //     }
-    //     while(fd_pas_trouve){
-    //         if(strcmp(emetteur,tab_cb[i]) == 0){
-    //             fd_pas_trouve = 0;
-    //             break;
-    //         }  
-    //         i++;
-    //     }
+    //     // while(fd_pas_trouve){
+    //         // char cb = tab_cb[i];
+    //         // printf("%d\n", cb);
+    //         // if(strcmp(emetteur,cb) == 0){
+    //         //     fd_pas_trouve = 0;
+    //         //     break;
+    //         // }  
+    //         // i++;
+    //     // }
     //     fd_toTerminal = Tab_fd_Term[i];
 
 
@@ -186,7 +182,8 @@ int main(int argc, char **argv) {
 
     pthread_t thread_auto;
     pthread_t tab_thread[nbTerminal];
-    arg_thread *args_auto  = malloc(sizeof(arg_thread_A));
+    arg_thread_A *args_auto  = malloc(sizeof(arg_thread_A));
+    arg_thread_T * args_term = malloc(sizeof(arg_thread_T));
     //---------------------------------------------------------------------- 
 
     // PIPES ENTRE ACQUISITION ET AUTORISATION
@@ -243,9 +240,8 @@ int main(int argc, char **argv) {
             sprintf(arg1,"%d", fd_pipeTerminalAcquisition[W][i]);
             sprintf(arg2,"%d", fd_pipeAcquisitionTerminal[R][i]);
 
-            // err_exec = execl( "/usr/bin/xterm", "/usr/bin/xterm", "-hold", "-e", "./Terminal", arg1, arg2, (char *)NULL );
-            // perror("Acquisition - initialisation : Le terminal s'est mal initialisé: ");    
-            exit(0);
+            err_exec = execl( "/usr/bin/xterm", "/usr/bin/xterm", "-hold", "-e", "./Terminal", arg1, arg2, (char *)NULL );
+            perror("Acquisition - initialisation : Le terminal s'est mal initialisé: ");    
         }
 
     }
@@ -255,23 +251,48 @@ int main(int argc, char **argv) {
     // Ceci reste le serveur d'acquisition
     printf("Avant la création des args du thread.\n");
 
-    
-    args_auto->fd_auto = 4;//fd_pipeAutorisationAcquisition[R];
-    // printf("1.\n");
-    // args_auto->Tab_fd_Term = Tab_fd_Term;
-    // printf("2.\n");
-    // args_auto->tab_cb = Tab_cb;
+    args_auto->fd_fromAuto = fd_pipeAutorisationAcquisition[R];
+    args_auto->Tab_fd_Term = Tab_fd_Term;
+    args_auto->tab_cb = Tab_cb;
 
     printf("Avant la création du thread.\n");
     if (pthread_create(&thread_auto, NULL, thread_LectureReponse, args_auto)) {
         perror("pthread_create");
         return EXIT_FAILURE;
     }
+    
+   
+    // Creation des threads lisant les terminaux
+    args_term->fd_toAuto = fd_pipeAcquisitionAutorisation[W];
+    args_term->Tab_fd_Term = Tab_fd_Term;
+    args_term->tab_cb = Tab_cb;
+
+    for (i = 0 ; i<nbTerminal ; i++){
+        args_term->fd_fromTerminal = fd_pipeTerminalAcquisition[R][i];
+        args_term->fd_toTerminal = fd_pipeAcquisitionTerminal[W][i];
+        args_term->i = i;
+        
+        if (pthread_create(&tab_thread[i], NULL, thread_LectureDemande, args_term)) {
+            perror("pthread_create");
+            return EXIT_FAILURE;
+        }
+    }
+
+    
+    for (i = 0 ; i<nbTerminal ; i++){
+        if (pthread_join(tab_thread[i], NULL)) {
+            perror("pthread_join");
+            return EXIT_FAILURE;
+        }
+    }
     if (pthread_join(thread_auto, NULL)) {
         perror("pthread_join");
         return EXIT_FAILURE;
     }
+     
         
-     printf("Après la création du thread.\n");
+    printf("Après la création du thread.\n");
+    free(args_auto);
+    free(args_term);
     return 0;
 }
